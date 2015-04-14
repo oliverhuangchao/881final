8dcc460826baf05a3ffc3088a1cc0450c0e02874
jinxuanqu <qujinxuan@gmail.com>
2015-04-13 23:10:46 -0400

    xml paser

--- xmlParse.php
+++ xmlParse.php
@@ -5,9 +5,29 @@ error_reporting (E_ALL | E_STRICT);
 
 $dom = new DOMDocument();
 $dom->load("test.xml");
- 
+/*
+shownode($dom->getElementsByTagName('docs')->item(0));
+function shownode($dom) {
+foreach ($dom->childNodes as $p)
+  if (hasChild($p)) {
+      	//echo $p->nodeName.' -> CHILDNODES<br>';
+      	shownode($p);
+  } elseif ($p->nodeType == XML_ELEMENT_NODE)
+  	if($p->nodeName=="title")
+   	echo $p->nodeName.' '.$p->nodeValue.'<br>';
+}
+function hasChild($p) {
+if ($p->hasChildNodes()) {
+  foreach ($p->childNodes as $c) {
+   if ($c->nodeType == XML_ELEMENT_NODE)
+    return true;
+  }
+}
+return false;
+}
+*/
 $xpath = new DOMXPath($dom);
-$elements = $xpath->query("/docs/p");
+$elements = $xpath->query("/docs/pp");
 if(!is_null($elements)) {
 	foreach($elements as $element) {
     //echo "<br/>[". $element->nodeName. "]";
@@ -15,7 +35,7 @@ if(!is_null($elements)) {
     $nodes = $element->childNodes;
     foreach ($nodes as $node) {
     	if ($node->nodeName == "title"){
-      		echo $node->nodeValue. "\n";
+      		echo $node->nodeValue.'<br>';
       		}
 
     }
